5ab23aff60093c894027921eba85233f9b4fd6a0
Hifizz <276161014@qq.com>
2019-08-31 17:05:26 +0800

    feat: middleware

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,15 +1,15 @@
-import {Injectable} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Cat } from './interface/cat.interface';
 
 @Injectable()
 export class CatsService {
-    private readonly cats: Cat[] = [];
+  private readonly cats: Cat[] = [];
 
-    create(cat: Cat) {
-        this.cats.push(cat);
-    }
+  create(cat: Cat) {
+    this.cats.push(cat);
+  }
 
-    findAll(): Cat[] {
-        return this.cats;
-    }
+  findAll(): Cat[] {
+    return this.cats;
+  }
 }
---
+++ src/common/middleware/logger.middleware.ts
@@ -0,0 +1,18 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+type Next = () => any;
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: Next) {
+    console.log('Request...', req.url);
+    next();
+  }
+}
+
+export function logger(req, res, next) {
+  // @ts-ignore
+  console.log('[Request]', req.url);
+  next();
+}
